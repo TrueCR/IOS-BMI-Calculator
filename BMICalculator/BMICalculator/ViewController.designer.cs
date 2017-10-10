bde06a85ba90e128a22d7fad36ff6a63ac5dd969
TrueCR <tylerg12@learn.cssd.ab.ca>
2017-10-10 13:25:00 -0600

    HELOS

--- BMICalculator/BMICalculator/ViewController.designer.cs
+++ BMICalculator/BMICalculator/ViewController.designer.cs
@@ -13,8 +13,43 @@ namespace BMICalculator
     [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel bmiLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton calculateButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField heightTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField weightTextField { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (bmiLabel != null) {
+                bmiLabel.Dispose ();
+                bmiLabel = null;
+            }
+
+            if (calculateButton != null) {
+                calculateButton.Dispose ();
+                calculateButton = null;
+            }
+
+            if (heightTextField != null) {
+                heightTextField.Dispose ();
+                heightTextField = null;
+            }
+
+            if (weightTextField != null) {
+                weightTextField.Dispose ();
+                weightTextField = null;
+            }
         }
     }
 }
